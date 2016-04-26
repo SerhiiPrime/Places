29db18b8a580398dfb35240b3727929ece0cfcc2
Serhii Onopriienko <onopriienko@edsson.com>
2016-04-26 17:46:35 +0300

    Add API router

---
+++ Places/APIRouter.swift
@@ -0,0 +1,42 @@
+//
+//  APIRouter.swift
+//  Places
+//
+//  Created by Serhii Onopriienko on 4/26/16.
+//  Copyright © 2016 Serhii Onopriienko. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+public enum APIRouter: URLRequestConvertible {
+    
+    case SearchVenues(Double, Double, String)
+
+    
+    public var URLRequest: NSMutableURLRequest {
+        let result: (path: String, method: Alamofire.Method, parameters: [String: AnyObject], encoding: Alamofire.ParameterEncoding, headers: [String: String]) = {
+            switch self {
+                
+            case .SearchVenues(let lat, let long, let query):
+                let params = [
+                    "client_id": Settings.API.clientID,
+                    "client_secret": Settings.API.clientSecret,
+                    "v": Settings.API.versionOfAPI,
+                    "ll": "\(lat),\(long)",
+                    "query": query
+                ]
+                return ("venues/search", .GET, params, .JSON, [:])
+            }
+        }()
+        
+        let baseURL = NSURL(string: Settings.API.baseURLString)
+        let URLRequest = NSMutableURLRequest(URL: baseURL!.URLByAppendingPathComponent(result.path))
+        URLRequest.HTTPMethod = result.method.rawValue
+        URLRequest.timeoutInterval = NSTimeInterval(60)
+        result.headers.forEach { URLRequest.setValue($1, forHTTPHeaderField: $0) }
+        
+        return result.encoding.encode(URLRequest, parameters: result.parameters).0
+    }
+}
---
+++ Places/Settings.swift
@@ -0,0 +1,19 @@
+//
+//  Settings.swift
+//  Places
+//
+//  Created by Serhii Onopriienko on 4/26/16.
+//  Copyright © 2016 Serhii Onopriienko. All rights reserved.
+//
+
+import Foundation
+
+struct Settings {
+    
+    struct API {
+        static let baseURLString = "https://api.foursquare.com/v2/"
+        static let clientID = "KC1V3HJV3AORNIIH4VXQM42NA4WYZOLAA5GXVZSJIGRCUN1Q"
+        static let clientSecret = "KON3QATMIHYOYZXI4IJPGCIZUVQACVSLH0S3RZYPIJYID5OX"
+        static let versionOfAPI = "20160215"
+    }
+}
