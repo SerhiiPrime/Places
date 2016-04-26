0d041de716c00378f6c6cb978e79374fff9ed63c
Serhii Onopriienko <onopriienko@edsson.com>
2016-04-26 21:32:55 +0300

    Add search controller

--- Places/PlacesViewController.swift
+++ Places/PlacesViewController.swift
@@ -18,20 +18,12 @@ class PlacesViewController: UIViewController {
         }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+}
+
+
+extension PlacesViewController: UISearchResultsUpdating {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        //filterString = searchController.searchBar.text ?? ""
     }
-    */
-
 }
